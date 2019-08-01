c6be7aa3e40821397f9a544cc03f61f9ffd90f30
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-08-01 22:37:23 +0530

    Add files via upload

---
+++ main/json.py
@@ -0,0 +1,19 @@
+education = {
+    "education": ["Education1","Education2"]
+}
+
+skills={
+    "skill": ["skill1","skill2"]
+}
+
+contact={
+    "phone": "p",
+    "address": "p",
+    "email": "p"
+}
+
+links={
+    "facebook": "https://www.facebook.com/",
+    "twitter": "https://twitter.com",
+    "github": "https://github.com/"
+}
