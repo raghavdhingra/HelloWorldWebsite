5f752953158de1e1bfda7d46e449931c8720c9d4
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-08-16 20:31:51 +0530

    Add files via upload

--- static/js/user.js
+++ static/js/user.js
@@ -1,1 +1,20 @@
 //User.js
+"use strict";
+let togVal = 0
+document.getElementById("leftPaneToggleBtn").addEventListener("click",()=>{
+    if (togVal == 0){
+        for ( var i = 1; i <= 5; i++ ){
+            document.getElementById(`left-active${i}`).classList.add("activeIt");
+        }
+        togVal+=1;
+        document.getElementById("turnAround").style.transition = "0.2s";
+        document.getElementById("turnAround").style.transform = "rotate(180deg)";
+    }
+    else{
+        for (var i = 1; i <= 5; i++ ){
+            document.getElementById(`left-active${i}`).classList.remove("activeIt");
+        }
+        togVal = 0;
+        document.getElementById("turnAround").style.transform = "rotate(0)";
+    }
+});
