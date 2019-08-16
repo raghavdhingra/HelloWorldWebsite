a093684dff37be55958033aaa2fe5b43d387b22c
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-08-16 20:27:33 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from main.models import TeamMember
+# Register your models here.
+admin.site.register(TeamMember)
---
+++ json.py
@@ -0,0 +1,23 @@
+def education():
+    return {
+        "education": ["Education1","Education2"]
+    }
+
+def skills():
+    return {
+        "skill": []
+    }
+
+def contact():
+    return {
+        "phone": "",
+        "address": "",
+        "email": ""
+    }
+
+def links():
+    return {
+        "facebook": "https://www.facebook.com/",
+        "twitter": "https://twitter.com/",
+        "github": "https://github.com/"
+    }
