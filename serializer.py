59813cdcd0b69f5aec62a062b67f74df4e955338
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-09-03 01:05:44 +0530

    Delete serializer.py

--- serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import TeamMember
-
-class MemberSerialiser(serializers.ModelSerializer):
-
-    class Meta:
-        model = TeamMember
-        # fields = ('user','FirstName','LastName','ProfilePicture','Authorised','BirthDate','Mail','created_date')
-        fields = '__all__'
